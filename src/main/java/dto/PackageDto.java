017b23f2afffe7ac1272962e472f65491b97d477
Ridho <ridhodhafi@gmail.com>
2022-12-27 12:55:44 +0700

    update terbaru

---
+++ src/main/java/dto/PackageDto.java
@@ -0,0 +1,45 @@
+//package dto;
+//
+//import jakarta.persistence.Id;
+//
+//public class PackageDto {
+//
+//	@Id
+//	private String idPackage;
+//	private String namaPackage;
+//	private Integer harga;
+//	private String idProduct;
+//	private String namaProduct;
+//	
+//	public String getIdPackage() {
+//		return idPackage;
+//	}
+//	public void setIdPackage(String idPackage) {
+//		this.idPackage = idPackage;
+//	}
+//	public String getNamaPackage() {
+//		return namaPackage;
+//	}
+//	public void setNamaPackage(String namaPackage) {
+//		this.namaPackage = namaPackage;
+//	}
+//	public Integer getHarga() {
+//		return harga;
+//	}
+//	public void setHarga(Integer harga) {
+//		this.harga = harga;
+//	}
+//	public String getIdProduct() {
+//		return idProduct;
+//	}
+//	public void setIdProduct(String idProduct) {
+//		this.idProduct = idProduct;
+//	}
+//	public String getNamaProduct() {
+//		return namaProduct;
+//	}
+//	public void setNamaProduct(String namaProduct) {
+//		this.namaProduct = namaProduct;
+//	}
+//	
+//}
